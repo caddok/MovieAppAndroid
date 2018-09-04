d089f6004132cb9eb0cef1243b61961d19685273
kevtimov <kaevtimov@gmail.com>
2018-09-04 18:50:38 +0300

    commit
    commit

--- app/src/main/java/com/example/georgi/movieapp/diconfig/StartModule.java
+++ app/src/main/java/com/example/georgi/movieapp/diconfig/StartModule.java
@@ -1,11 +1,8 @@
 package com.example.georgi.movieapp.diconfig;
 
-<<<<<<< HEAD
 import com.example.georgi.movieapp.views.views.start.MovieStartContracts;
-=======
 import com.example.georgi.movieapp.views.views.movielist.MovieListPresenter;
 import com.example.georgi.movieapp.views.views.movielist.MoviesListContracts;
->>>>>>> 42bc10ef6a9eb6ae4c1d21fe49de203ef2ba29f6
 import com.example.georgi.movieapp.views.views.start.StartActivityFragment;
 import com.example.georgi.movieapp.views.views.start.StartPresenter;
 
@@ -22,9 +19,5 @@ public abstract class StartModule {
 
     @ActivityScoped
     @Binds
-<<<<<<< HEAD
     abstract MovieStartContracts.Presenter createPresenter(StartPresenter presenter);
-=======
-    abstract MoviesListContracts.Presenter listPresenter(MovieListPresenter presenter);
->>>>>>> 42bc10ef6a9eb6ae4c1d21fe49de203ef2ba29f6
 }
