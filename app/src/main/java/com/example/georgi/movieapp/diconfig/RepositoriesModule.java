3ef4439781df7ec284408f75842fac74a8e368a1
kevtimov <kaevtimov@gmail.com>
2018-09-04 15:21:38 +0300

    add singleton

--- app/src/main/java/com/example/georgi/movieapp/diconfig/RepositoriesModule.java
+++ app/src/main/java/com/example/georgi/movieapp/diconfig/RepositoriesModule.java
@@ -7,12 +7,16 @@ import com.example.georgi.movieapp.parsers.base.JsonParser;
 import com.example.georgi.movieapp.repositories.InMemoryHttpMovieRepository;
 import com.example.georgi.movieapp.repositories.Repository;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class RepositoriesModule {
+
     @Provides
+    @Singleton
     public Repository<Movie> movieRepository(HttpRequester requester, JsonParser parser) {
         String url = Constants.BASE_SERVER_URL + "/movies";
         return new InMemoryHttpMovieRepository(url,requester,parser);
